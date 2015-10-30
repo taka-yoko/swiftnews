4b242eefde717e4e018d33d7231e7de1fd89917d
tamoriinu <tamoriinu@gmail.com>
2015-10-30 22:43:49 +0900

    7-3

---
+++ SwiftNews/DetailController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailController.swift
+//  SwiftNews
+//
+//  Created by tyoko on 2015/10/25.
+//  Copyright © 2015年 tyoko. All rights reserved.
+//
+
+import UIKit
+
+class DetailController: UIViewController {
+    
+}
--- SwiftNews/ViewController.swift
+++ SwiftNews/ViewController.swift
@@ -8,7 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    let newsUrlString = "http://www.apple.com/"
+    
+    @IBAction func refresh(sender: AnyObject) {
+        
+        let url = NSURL(string: newsUrlString)!
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: {data, response, error in
+            print("done, length \(data!.length)")
+        })
+        
+        task.resume()
+        
+    }
+    
+    
+    
+    
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("news")
+        
+        cell!.textLabel?.text = "Swift News"
+        
+        return cell!
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
